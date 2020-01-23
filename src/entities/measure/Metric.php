22053dd904d65c60e801856c7892fce4a10ee288
Andrew Gardener <andrew.e.gardener@gmail.com>
2020-01-23 12:45:37 -0800

    Add Use Metrics test
    - Reordered metrics

--- src/entities/measure/Metric.php
+++ src/entities/measure/Metric.php
@@ -6,8 +6,8 @@ use IMSGlobal\Caliper;
 class Metric extends Caliper\util\BasicEnum {
     const
         __default = '',
-        ASSESSMENTS_SUBMITTED = 'AssessmentsSubmitted',
         ASSESSMENTS_PASSED = 'AssessmentsPassed',
+        ASSESSMENTS_SUBMITTED = 'AssessmentsSubmitted',
         MINUTES_ON_TASK = 'MinutesOnTask',
         SKILLS_MASTERED = 'SkillsMastered',
         STANDARDS_MASTERED = 'StandardsMastered',
---
+++ tests/constants/EnvelopeTermMetricTest.php
@@ -0,0 +1,35 @@
+<?php
+require_once __DIR__ . '/../CaliperTestCase.php';
+
+use IMSGlobal\Caliper\entities\measure\AggregateMeasure;
+use IMSGlobal\Caliper\entities\measure\Metric;
+
+class EnvelopeTermMetricTest extends CaliperTestCase {
+    function setUp() {
+        $this->setFixtureRelativeSubDirectoryPath('/constantsFixtures');
+        parent::setUp();
+
+        $metrics = array(
+            new Metric(Metric::ASSESSMENTS_PASSED),
+            new Metric(Metric::ASSESSMENTS_SUBMITTED),
+            new Metric(Metric::MINUTES_ON_TASK),
+            new Metric(Metric::SKILLS_MASTERED),
+            new Metric(Metric::STANDARDS_MASTERED),
+            new Metric(Metric::UNITS_COMPLETED),
+            new Metric(Metric::UNITS_PASSED),
+            new Metric(Metric::WORDS_READ),
+        );
+
+        $data = array();
+        foreach ($metrics as $metric) {
+            $data[]= ( AggregateMeasure::makeAnonymous() )
+                ->setMetric( $metric );
+        }
+
+        $this->setTestObject((new \IMSGlobal\Caliper\request\Envelope())
+            ->setSensorId(new \IMSGlobal\Caliper\Sensor('https://example.edu/sensors/1'))
+            ->setSendTime(new \DateTime('2020-01-22T10:05:00.000Z'))
+            ->setData($data)
+        );
+    }
+}

bdcd0ab48a4e25c732b19ee97ac11105ddaeb6be
Andrew Gardener <andrew.e.gardener@gmail.com>
2020-01-23 12:39:03 -0800

    Support constant fixture tests
    allows adding a sub directory path for tests (will also be useful in the future for `commonErrorFixtures` directory)

--- tests/EntityPersonAnonymousTest.php
+++ tests/EntityPersonAnonymousTest.php
@@ -1,11 +1,7 @@
 <?php
 require_once 'CaliperTestCase.php';
 
-use IMSGlobal\Caliper\entities\agent\SoftwareApplication;
-use IMSGlobal\Caliper\entities\SystemIdentifier;
-use IMSGlobal\Caliper\entities\SystemIdentifierType;
 use IMSGlobal\Caliper\entities\agent\Person;
-
 
 /**
  * @requires PHP 5.6.28

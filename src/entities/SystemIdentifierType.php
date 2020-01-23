b7ea3aafcbd7f633c005b6f7e60b90f602c50d5c
Andrew Gardener <andrew.e.gardener@gmail.com>
2020-01-23 12:44:58 -0800

    Add System Identifier Types test
    - Reordered types
    - Fixed typo in LtiToolId

--- src/entities/SystemIdentifierType.php
+++ src/entities/SystemIdentifierType.php
@@ -6,16 +6,16 @@ use IMSGlobal\Caliper;
 class SystemIdentifierType extends Caliper\util\BasicEnum {
     const
         __default = '',
+        ACCOUNT_USERNAME = 'AccountUserName',
+        EMAIL_ADDRESS = 'EmailAddress',
         LIS_SOURCED_ID = 'LisSourcedId',
-        ONEROSTER_SOURCED_ID = 'OneRosterSourcedId',
-        SIS_SOURCEDID = 'SisSourcedId',
         LTI_CONTEXT_ID = 'LtiContextId',
         LTI_DEPLOYMENT_ID = 'LtiDeploymentId',
         LTI_PLATFORM_ID = 'LtiPlatformId',
-        LTI_TOOL_ID = 'LtiToolid',
+        LTI_TOOL_ID = 'LtiToolId',
         LTI_USERID = 'LtiUserId',
-        EMAIL_ADDRESS = 'EmailAddress',
-        ACCOUNT_USERNAME = 'AccountUserName',
-        SYSTEM_ID = 'SystemId',
-        OTHER = 'Other';
+        ONEROSTER_SOURCED_ID = 'OneRosterSourcedId',
+        OTHER = 'Other',
+        SIS_SOURCEDID = 'SisSourcedId',
+        SYSTEM_ID = 'SystemId';
 }
---
+++ tests/constants/EnvelopeTermSystemIdentifierTypeTest.php
@@ -0,0 +1,40 @@
+<?php
+require_once __DIR__ . '/../CaliperTestCase.php';
+
+use IMSGlobal\Caliper\entities\Entity;
+use IMSGlobal\Caliper\entities\SystemIdentifier;
+use IMSGlobal\Caliper\entities\SystemIdentifierType;
+
+class EnvelopeTermSystemIdentifierTypeTest extends CaliperTestCase {
+    function setUp() {
+        $this->setFixtureRelativeSubDirectoryPath('/constantsFixtures');
+        parent::setUp();
+
+        $system_identifiers = array(
+            new SystemIdentifier('root',  new SystemIdentifierType(SystemIdentifierType::ACCOUNT_USERNAME)),
+            new SystemIdentifier('jane@example.edu', new SystemIdentifierType(SystemIdentifierType::EMAIL_ADDRESS)),
+            new SystemIdentifier('example.edu:SI182-F16', new SystemIdentifierType(SystemIdentifierType::LIS_SOURCED_ID)),
+            new SystemIdentifier('example.edu:CI182-S16', new SystemIdentifierType(SystemIdentifierType::LTI_CONTEXT_ID)),
+            new SystemIdentifier('deployment_id_1', new SystemIdentifierType(SystemIdentifierType::LTI_DEPLOYMENT_ID)),
+            new SystemIdentifier('platform_id_1', new SystemIdentifierType(SystemIdentifierType::LTI_PLATFORM_ID)),
+            new SystemIdentifier('tool_id_1', new SystemIdentifierType(SystemIdentifierType::LTI_TOOL_ID)),
+            new SystemIdentifier('user_id_1', new SystemIdentifierType(SystemIdentifierType::LTI_USERID)),
+            new SystemIdentifier('one_roster_id_1', new SystemIdentifierType(SystemIdentifierType::ONEROSTER_SOURCED_ID)),
+            new SystemIdentifier('other_1', new SystemIdentifierType(SystemIdentifierType::OTHER)),
+            new SystemIdentifier('sis_sourcedid_1', new SystemIdentifierType(SystemIdentifierType::SIS_SOURCEDID)),
+            new SystemIdentifier('system_id_1', new SystemIdentifierType(SystemIdentifierType::SYSTEM_ID))
+        );
+
+        $data = array();
+        foreach ($system_identifiers as $system_identifier) {
+            $data[]= ( Entity::makeAnonymous() )
+                ->setOtherIdentifiers( [$system_identifier] );
+        }
+
+        $this->setTestObject((new \IMSGlobal\Caliper\request\Envelope())
+            ->setSensorId(new \IMSGlobal\Caliper\Sensor('https://example.edu/sensors/1'))
+            ->setSendTime(new \DateTime('2020-01-22T10:05:00.000Z'))
+            ->setData($data)
+        );
+    }
+}
